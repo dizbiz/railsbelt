8edd84d9ca90679107f577d8036d0ae03ed866eb
Daniel Burstyn <danielgburstyn@gmail.com>
2016-12-14 10:44:51 -0600

    changed to match live version

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ActiveRecord::Base
   has_many :events, through: :event_rosters, dependent: :destroy
   has_many :events
   #belongs_to :host, class_name: 'User', foreign_key: 'user_id'
+	EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
+	validates :first_name, :last_name, presence: true, length: { in: 2..20 }
+	validates :email_address, presence: true, uniqueness: { case_sensitive: false }, format: { with: EMAIL_REGEX }
+	validates :age, numericality: { greater_than: 10, less_than: 150}
+	before_save do
+		self.email_address.downcase!
+	end
+  before_create do
+    self.admin = false #assuming there is an admin field with a boolean value
+  end
+  def full_name
+  	self.first_name + " " + self.last_name
+  end
+  def admin?
+  	self.admin
+  end
 end
